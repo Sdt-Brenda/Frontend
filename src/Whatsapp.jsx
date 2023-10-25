acfdcdd3bc233cf884db0890ab9465207837c8f8
Maciel de Lima Marcos <136296059+MarcosMaciel2@users.noreply.github.com>
2023-10-25 03:48:10 -0300

    Add files via upload

---
+++ src/Whatsapp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./styles/Whatsapp.module.css";
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+const WhatsAppButton = () => {
+  return (
+    <a href="https://wa.me/xxxxxxxxxx" className={styles.whatsappButton}>
+      <WhatsAppIcon />
+    </a>
+  );
+};
+
+export default WhatsAppButton;
